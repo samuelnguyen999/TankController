bf9f982a551d21637a2a535c70098f9f5ff8d74f
Samuel Nguyen <samuel.nguyen@wallawalla.edu>
2024-02-16 15:39:38 -0800

    save

--- extras/web_view/lib/main.dart
+++ extras/web_view/lib/main.dart
@@ -13,22 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tank Monitor',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 58, 104, 183)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Tank Monitor'),
@@ -92,17 +78,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: GridView.count(
           crossAxisCount: 4,
           children: List.generate(
-            10,
+            24,
             (index) {
               return Container(
-                color: Colors.blue,
+                color: Color.fromARGB(255, 113, 210, 128),
                 margin: const EdgeInsets.all(10),
                 child: Center(
                   child: Text(
                     'Tank $index',
                     style: const TextStyle(
                       color: Colors.white,
-                      fontSize: 20,
+                      fontSize: 25,
                     ),
                   ),
                 ),
