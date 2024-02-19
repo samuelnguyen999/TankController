c043e82c9c7e87c62d99be260b5d17facbcace20
Samuel Nguyen <samuel.nguyen@wallawalla.edu>
2024-02-19 15:37:32 -0800

    grid builder for dynamic item count

--- extras/web_view/lib/main.dart
+++ extras/web_view/lib/main.dart
@@ -56,64 +56,75 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
+    // This method is rerun every time setState is called to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: GridView.count(
-          crossAxisCount: 4,
-          children: List.generate(
-            24,
-            (index) {
-              return Container(
-                color: Color.fromARGB(255, 113, 210, 128),
-                margin: const EdgeInsets.all(10),
-                child: Center(
-                  child: Text(
-                    'Tank $index',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                    ),
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4, // number of items in each row
+          mainAxisSpacing: 8.0, // spacing between rows
+          crossAxisSpacing: 8.0, // spacing between columns
+        ),
+        padding: const EdgeInsets.all(0.0), // padding around the grid
+        itemCount: 24, //items.length, // total number of items
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {},
+            // child: Ink(
+            //   width: 200,
+            //   height: 200,
+            //   color: Colors.blue,
+            // ),
+            child: Container(
+              color: Color.fromARGB(255, 113, 210, 128), // color of grid items
+              child: Center(
+                child: Text(
+                  'Tank $index', //items[index],
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
                   ),
                 ),
-              );
-            },
+              ),
+            ),
+          );
+        },
+      )
+          // child: GridView.count(
+          //   crossAxisCount: 4,
+          //   children: List.generate(
+          //     24,
+          //     (index) {
+          //       return Container(
+          //         color: Color.fromARGB(255, 113, 210, 128),
+          //         margin: const EdgeInsets.all(10),
+          //         child: Center(
+          //           child: Text(
+          //             'Tank $index',
+          //             style: const TextStyle(
+          //               color: Colors.white,
+          //               fontSize: 25,
+          //             ),
+          //           ),
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // ),
           ),
-        ),
-      ),
     );
   }
 }
-          // child: Text('Hello World'),
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
          
   //         mainAxisAlignment: MainAxisAlignment.center,
   //         children: <Widget>[
