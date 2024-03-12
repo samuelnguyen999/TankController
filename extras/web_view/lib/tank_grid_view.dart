7088572a923e8a0d4bbdb662c4f8f1d613fd902d
Samuel Nguyen <samuel.nguyen@wallawalla.edu>
2024-03-12 15:02:42 -0700

    update

--- extras/web_view/lib/tank_grid_view.dart
+++ extras/web_view/lib/tank_grid_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:http/http.dart' as http;
 import 'package:web_view/data_log.dart';
 import 'package:web_view/main.dart';
 
@@ -16,6 +17,14 @@ class TankGridView extends State<MyHomePage> {
     //   'tankId' : log.tankId,
   };
 
+  Future<dynamic> getData() async {
+    var url = Uri.https('https://oap.cs.wallawalla.edu/logs/');
+    http.Response response = await http.get(url);
+    debugPrint('Response status: ${response.statusCode}');
+    debugPrint('Response body: ${response.body}');
+    return response;
+  }
+
   // Function to be called on click
   void _onTileClicked(int index) {
     debugPrint("You clicked on tank $index");
@@ -26,175 +35,204 @@ class TankGridView extends State<MyHomePage> {
               appBar: AppBar(
                   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
                   title: Text('Tank $index Page')),
-              body: SingleChildScrollView(
-                child: Container(
-                  // Use background color to emphasize that it's a new route.
-                  //color: Colors.lightBlueAccent,
-                  padding: const EdgeInsets.all(8),
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.95,
-                        //height: MediaQuery.of(context).size.width * 0.95 * 0.65,
-                        //margin: const EdgeInsets.all(25.0),
-                        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blueAccent),
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Table(
-                          border: TableBorder.all(
-                              color:
-                                  Theme.of(context).colorScheme.inversePrimary),
-                          children: [
-                            TableRow(children: [
-                              const Text('Time'),
-                              Text('time of tank $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('Tank ID'),
-                              Text('tankID of tank $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('Current Temperature'),
-                              Text('currentTemperatureString of $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('Thermal Target'),
-                              Text('Thermal Target of $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('Current pH'),
-                              Text('currentPhString of $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('pH Target'),
-                              Text('pHTarget of $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('On Time'),
-                              Text('onTime (millis() / 1000) of $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('KP (Proportional gain)'),
-                              Text('kp of $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('KI (Integral gain)'),
-                              Text('ki of $index'),
-                            ]),
-                            TableRow(children: [
-                              const Text('KD (Derivative gain)'),
-                              Text('kd of $index'),
-                            ])
-                          ],
-                        ),
-                      ),
-                      ListTile(
-                        title: const Text(
-                          '1625 Main Street',
-                          style: TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                        subtitle: const Text('My City, CA 99984'),
-                        leading: Icon(
-                          Icons.restaurant_menu,
-                          color: Colors.blue[500],
-                        ),
-                      ),
-                      const Divider(),
-                      ListTile(
-                        title: const Text(
-                          '(408) 555-1212',
-                          style: TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                        leading: Icon(
-                          Icons.contact_phone,
-                          color: Colors.blue[500],
-                        ),
-                      ),
-                      ListTile(
-                        title: const Text('costa@example.com'),
-                        leading: Icon(
-                          Icons.contact_mail,
-                          color: Colors.blue[500],
-                        ),
-                      ),
-                      Text(
-                        "Variable Graph of tank $index",
-                        style: const TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20.0,
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.95,
-                        height: MediaQuery.of(context).size.width * 0.95 * 0.65,
-                        //margin: const EdgeInsets.all(5.0),
-                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blueAccent),
-                          //color: Colors.red,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: LineChart(
-                          LineChartData(
-                            //lineTouchData:LineTouchData(touchTooltipData: LineTouchTooltipData(getTooltipItems:))
-                            lineBarsData: [
-                              LineChartBarData(
-                                spots: const [
-                                  FlSpot(0, 4),
-                                  FlSpot(1, 7),
-                                  FlSpot(2, 2),
-                                  FlSpot(3, 9),
-                                  FlSpot(4, 5),
-                                  FlSpot(5, 10),
-                                  FlSpot(6, 1),
-                                  FlSpot(7, 4),
-                                  FlSpot(8, 9),
-                                  FlSpot(9, 8),
-                                  FlSpot(10, 3),
-                                ],
-                                isCurved: true,
-                                dotData: const FlDotData(show: true),
-                                color: Colors.blue,
-                                barWidth: 3,
-                                belowBarData: BarAreaData(
-                                  show: true,
-                                  gradient: LinearGradient(
-                                    begin: Alignment.bottomLeft,
-                                    end: Alignment.topRight,
-                                    colors: <Color>[
-                                      Colors.cyan,
-                                      Colors.green.withOpacity(0.4)
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                            minX: 0,
-                            maxX: 10,
-                            minY: 0,
-                            maxY: 10,
-                            backgroundColor: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                    // child: const Hero(
-                    //   tag: 'selection',
-                    //   child: SizedBox(
-                    //     width: 100,
-                    //     // child: Image.asset(
-                    //     //   'lib/assets/tank-icon.png',
-                    //     // ),
-                    //   ),
-                    // ),
-                  ),
-                ),
-              ));
+              body: myFutureBuilder(context, index));
         },
+      ),
+    );
+  }
+
+  FutureBuilder<dynamic> myFutureBuilder(BuildContext context, int index) {
+    return FutureBuilder<dynamic>(
+      future: getData(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasError) {
+          return Container(
+            // width: MediaQuery.of(context).size.width,
+            // margin: const EdgeInsets.all(20),
+            // padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+            // decoration: BoxDecoration(
+            //   border: Border.all(color: Colors.blueAccent),
+            //   color: Colors.white,
+            //   borderRadius: BorderRadius.circular(20),
+            // ),
+            child: const Text('There was an error..'),
+          );
+        } else if (snapshot.hasData) {
+          return SingleChildScrollView(
+            child: Container(
+              // Use background color to emphasize that it's a new route.
+              //color: Colors.lightBlueAccent,
+              padding: const EdgeInsets.all(8),
+              alignment: Alignment.topLeft,
+              child: Column(
+                children: [
+                  const Text('Received data..'),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    //height: MediaQuery.of(context).size.width * 0.95 * 0.65,
+                    //margin: const EdgeInsets.all(25.0),
+                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: myTable(context, index),
+                  ),
+                  ListTile(
+                    title: const Text(
+                      '1625 Main Street',
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    subtitle: const Text('My City, CA 99984'),
+                    leading: Icon(
+                      Icons.restaurant_menu,
+                      color: Colors.blue[500],
+                    ),
+                  ),
+                  const Divider(),
+                  ListTile(
+                    title: const Text(
+                      '(408) 555-1212',
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    leading: Icon(
+                      Icons.contact_phone,
+                      color: Colors.blue[500],
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('costa@example.com'),
+                    leading: Icon(
+                      Icons.contact_mail,
+                      color: Colors.blue[500],
+                    ),
+                  ),
+                  Text(
+                    "Variable Graph of tank $index",
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    height: MediaQuery.of(context).size.width * 0.95 * 0.65,
+                    //margin: const EdgeInsets.all(5.0),
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent),
+                      //color: Colors.red,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: myChart(),
+                  ),
+                ],
+                // child: const Hero(
+                //   tag: 'selection',
+                //   child: SizedBox(
+                //     width: 100,
+                //     // child: Image.asset(
+                //     //   'lib/assets/tank-icon.png',
+                //     // ),
+                //   ),
+                // ),
+              ),
+            ),
+          );
+        }
+        throw '';
+      },
+    );
+  }
+
+  Table myTable(BuildContext context, int index) {
+    return Table(
+      border:
+          TableBorder.all(color: Theme.of(context).colorScheme.inversePrimary),
+      children: [
+        TableRow(children: [
+          const Text('Time'),
+          Text('time of tank $index'),
+        ]),
+        TableRow(children: [
+          const Text('Tank ID'),
+          Text('tankID of tank $index'),
+        ]),
+        TableRow(children: [
+          const Text('Current Temperature'),
+          Text('currentTemperatureString of $index'),
+        ]),
+        TableRow(children: [
+          const Text('Thermal Target'),
+          Text('Thermal Target of $index'),
+        ]),
+        TableRow(children: [
+          const Text('Current pH'),
+          Text('currentPhString of $index'),
+        ]),
+        TableRow(children: [
+          const Text('pH Target'),
+          Text('pHTarget of $index'),
+        ]),
+        TableRow(children: [
+          const Text('On Time'),
+          Text('onTime (millis() / 1000) of $index'),
+        ]),
+        TableRow(children: [
+          const Text('KP (Proportional gain)'),
+          Text('kp of $index'),
+        ]),
+        TableRow(children: [
+          const Text('KI (Integral gain)'),
+          Text('ki of $index'),
+        ]),
+        TableRow(children: [
+          const Text('KD (Derivative gain)'),
+          Text('kd of $index'),
+        ])
+      ],
+    );
+  }
+
+  LineChart myChart() {
+    return LineChart(
+      LineChartData(
+        //lineTouchData:LineTouchData(touchTooltipData: LineTouchTooltipData(getTooltipItems:))
+        lineBarsData: [
+          LineChartBarData(
+            spots: const [
+              FlSpot(0, 4),
+              FlSpot(1, 7),
+              FlSpot(2, 2),
+              FlSpot(3, 9),
+              FlSpot(4, 5),
+              FlSpot(5, 10),
+              FlSpot(6, 1),
+              FlSpot(7, 4),
+              FlSpot(8, 9),
+              FlSpot(9, 8),
+              FlSpot(10, 3),
+            ],
+            isCurved: true,
+            dotData: const FlDotData(show: true),
+            color: Colors.blue,
+            barWidth: 3,
+            belowBarData: BarAreaData(
+              show: true,
+              gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: <Color>[Colors.cyan, Colors.green.withOpacity(0.4)],
+              ),
+            ),
+          ),
+        ],
+        minX: 0,
+        maxX: 10,
+        minY: 0,
+        maxY: 10,
+        backgroundColor: Colors.white,
       ),
     );
   }
